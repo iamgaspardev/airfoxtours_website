7aca638bef3fb6f811b5e19f6c99879750c319e7
gaspar-dev <kalugendosmazima@gmail.com>
2024-06-13 21:08:08 +0300

    udates checks

---
+++ resources/views/components/whyus.blade.php
@@ -0,0 +1,58 @@
+
+</section>
+    <section id="features" class="features">
+  <div class="container">
+
+    <div class="section-title" data-aos="fade-up">
+      <h2><strong>Why Choose Airfox Tours</strong></h2>
+      <p>At Airfox Tours, we create unique opportunities for all our customers, ensuring an unforgettable travel experience tailored to your desires. Discover the exceptional features we offer that set us apart from the rest.</p>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4 mb-5 mb-lg-0" data-aos="fade-right">
+        <ul class="nav nav-tabs flex-column">
+          <li class="nav-item">
+            <a class="nav-link active show" data-bs-toggle="tab" href="#tab-1">
+              <h4>Customizable Itineraries</h4>
+              <p>Personalize your travel plans to fit your schedule and interests with our flexible itinerary options.</p>
+            </a>
+          </li>
+          <li class="nav-item mt-2">
+            <a class="nav-link" data-bs-toggle="tab" href="#tab-2">
+              <h4>Exclusive Destinations</h4>
+              <p>Explore hidden gems and exclusive locations that you won't find in standard travel packages.</p>
+            </a>
+          </li>
+          <li class="nav-item mt-2">
+            <a class="nav-link" data-bs-toggle="tab" href="#tab-3">
+              <h4>Premium Support</h4>
+              <p>Enjoy 24/7 customer support from our dedicated team, ensuring a smooth and worry-free journey.</p>
+            </a>
+          </li>
+          
+        </ul>
+      </div>
+      <div class="col-lg-7 ml-auto" data-aos="fade-left" data-aos-delay="100">
+        <div class="tab-content">
+          <div class="tab-pane active show" id="tab-1">
+            <figure>
+              <img src="assets/img/airplane2.png" alt="Customizable Itineraries" class="img-fluid">
+            </figure>
+          </div>
+          <div class="tab-pane" id="tab-2">
+            <figure>
+              <img src="assets/img/airplane1.png" alt="Exclusive Destinations" class="img-fluid">
+            </figure>
+          </div>
+          <div class="tab-pane" id="tab-3">
+            <figure>
+              <img src="assets/img/airplane3.png" alt="Premium Support" class="img-fluid">
+            </figure>
+          </div>
+        
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -147,7 +147,9 @@
 
             </div>
         </section><!-- End Services Section -->
-
+        <div>
+            @include('components.whyus')
+        </div>
         <!-- ======= Portfolio Section ======= -->
         <section id="portfolio" class="portfolio">
             <div class="container">
@@ -208,79 +210,79 @@
                     @include('team-members', ['teamMembers' => $teamMembers])
                 </div>
                 <!-- <div class="row">
-                                        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                                            <div class="member" data-aos="fade-up">
-                                                <div class="member-img">
-                                                    <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
-                                                    <div class="social">
-                                                        <a href=""><i class="bi bi-twitter"></i></a>
-                                                        <a href=""><i class="bi bi-facebook"></i></a>
-                                                        <a href=""><i class="bi bi-instagram"></i></a>
-                                                        <a href=""><i class="bi bi-linkedin"></i></a>
+                                                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                                                    <div class="member" data-aos="fade-up">
+                                                        <div class="member-img">
+                                                            <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
+                                                            <div class="social">
+                                                                <a href=""><i class="bi bi-twitter"></i></a>
+                                                                <a href=""><i class="bi bi-facebook"></i></a>
+                                                                <a href=""><i class="bi bi-instagram"></i></a>
+                                                                <a href=""><i class="bi bi-linkedin"></i></a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="member-info">
+                                                            <h4>Mr Fuad Ally</h4>
+                                                            <span>Chief Executive Officer</span>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="member-info">
-                                                    <h4>Mr Fuad Ally</h4>
-                                                    <span>Chief Executive Officer</span>
-                                                </div>
-                                            </div>
-                                        </div>
 
-                                        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                                            <div class="member" data-aos="fade-up" data-aos-delay="100">
-                                                <div class="member-img">
-                                                    <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
-                                                    <div class="social">
-                                                        <a href=""><i class="bi bi-twitter"></i></a>
-                                                        <a href=""><i class="bi bi-facebook"></i></a>
-                                                        <a href=""><i class="bi bi-instagram"></i></a>
-                                                        <a href=""><i class="bi bi-linkedin"></i></a>
+                                                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                                                    <div class="member" data-aos="fade-up" data-aos-delay="100">
+                                                        <div class="member-img">
+                                                            <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
+                                                            <div class="social">
+                                                                <a href=""><i class="bi bi-twitter"></i></a>
+                                                                <a href=""><i class="bi bi-facebook"></i></a>
+                                                                <a href=""><i class="bi bi-instagram"></i></a>
+                                                                <a href=""><i class="bi bi-linkedin"></i></a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="member-info">
+                                                            <h4>Ummyhadjat Othuman</h4>
+                                                            <span>Marketing Director</span>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="member-info">
-                                                    <h4>Ummyhadjat Othuman</h4>
-                                                    <span>Marketing Director</span>
-                                                </div>
-                                            </div>
-                                        </div>
 
-                                        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                                            <div class="member" data-aos="fade-up" data-aos-delay="200">
-                                                <div class="member-img">
-                                                    <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
-                                                    <div class="social">
-                                                        <a href=""><i class="bi bi-twitter"></i></a>
-                                                        <a href=""><i class="bi bi-facebook"></i></a>
-                                                        <a href=""><i class="bi bi-instagram"></i></a>
-                                                        <a href=""><i class="bi bi-linkedin"></i></a>
+                                                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                                                    <div class="member" data-aos="fade-up" data-aos-delay="200">
+                                                        <div class="member-img">
+                                                            <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
+                                                            <div class="social">
+                                                                <a href=""><i class="bi bi-twitter"></i></a>
+                                                                <a href=""><i class="bi bi-facebook"></i></a>
+                                                                <a href=""><i class="bi bi-instagram"></i></a>
+                                                                <a href=""><i class="bi bi-linkedin"></i></a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="member-info">
+                                                            <h4>Mr Fuad Ally</h4>
+                                                            <span>Chief Executive Officer</span>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="member-info">
-                                                    <h4>Mr Fuad Ally</h4>
-                                                    <span>Chief Executive Officer</span>
-                                                </div>
-                                            </div>
-                                        </div>
 
-                                        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-                                            <div class="member" data-aos="fade-up" data-aos-delay="300">
-                                                <div class="member-img">
-                                                    <img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">
-                                                    <div class="social">
-                                                        <a href=""><i class="bi bi-twitter"></i></a>
-                                                        <a href=""><i class="bi bi-facebook"></i></a>
-                                                        <a href=""><i class="bi bi-instagram"></i></a>
-                                                        <a href=""><i class="bi bi-linkedin"></i></a>
+                                                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                                                    <div class="member" data-aos="fade-up" data-aos-delay="300">
+                                                        <div class="member-img">
+                                                            <img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">
+                                                            <div class="social">
+                                                                <a href=""><i class="bi bi-twitter"></i></a>
+                                                                <a href=""><i class="bi bi-facebook"></i></a>
+                                                                <a href=""><i class="bi bi-instagram"></i></a>
+                                                                <a href=""><i class="bi bi-linkedin"></i></a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="member-info">
+                                                            <h4>Ummyhadjat Othuman</h4>
+                                                            <span>Marketing Director</span>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="member-info">
-                                                    <h4>Ummyhadjat Othuman</h4>
-                                                    <span>Marketing Director</span>
-                                                </div>
-                                            </div>
-                                        </div>
 
-                                    </div> -->
+                                            </div> -->
 
             </div>
         </section><!-- End Our Team Section -->
